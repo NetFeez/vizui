ef5800705ad233dd0c979962361c015536677c1f
NetFeez <netfeez.dev@gmail.com>
2026-05-08 05:00:03 +0000

    fix(scripts): update build script to improve error handling and output logging

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-if npx tsc --pretty true > .compile.log 2>&1; then
+set -eo pipefail
+outputFile=output.log
+
+if npx tsc > $outputFile 2>&1; then
     echo -e "\x1b[32mCompilation successful.\x1b[0m"
 else
-    echo -e "\x1b[31mCompilation failed. Check \x1b[36m.compile.log\x1b[31m for details.\x1b[0m"
-    tail .compile.log
+    echo -e "\x1b[31mCompilation failed. Check \x1b[36m$outputFile\x1b[31m for details.\x1b[0m"
+    tail $outputFile
 fi
+
+rm -f $outputFile
